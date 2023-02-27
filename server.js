2e05e1ea1af24e97bbaf04589ebada2a2d409e53
newby101 <ishimwekevin45@gmail.com>
2023-02-27 19:24:36 +0200

    m

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const { User } = require("./models");
 const useRoutes = require("./routes/userRoutes");
 const routes = require("./routes/userInfoRoutes");
 app.get("/", (req, res) => {
